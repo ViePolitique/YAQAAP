d9c316b2d3410d2e6cc82bff7c3eaa6de8b823c4
Filimindji <dh-alias@outlook.com>
2015-07-20 20:22:45 +0200

    Ajout initial

---
+++ Yaqaap/Yaqaap/Yaqaap.ServiceInterface/MyServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using Yaqaap.ServiceModel;
+
+namespace Yaqaap.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+
+        public object Any(Search request)
+        {
+            return new HelloResponse { Result = "No similar question found... yet !" };
+        }
+    }
+}
---
+++ Yaqaap/Yaqaap/Yaqaap.ServiceModel/Search.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+
+namespace Yaqaap.ServiceModel
+{
+    [Route("/search/{Question}")]
+    public class Search : IReturn<SearchResponse>
+    {
+        public string Question { get; set; }
+    }
+    
+    public class SearchResponse
+    {
+        public string Result { get; set; }
+    }
+}
