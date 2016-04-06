ac14acf258b96985ce18dfb5db6aee02a50a8f05
Filimindji <dh-alias@outlook.com>
2016-04-06 22:25:04 +0200

    Call gulp after deploiement

--- deploy.cmd
+++ deploy.cmd
@@ -94,25 +94,24 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 
 IF !ERRORLEVEL! NEQ 0 goto error
 
-:: 3. Call gulp
-IF EXIST "%DEPLOYMENT_TEMP%\gulpfile.js" (
-  pushd %DEPLOYMENT_TEMP%
-  echo Execute gulp
-  call gulp
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-) ELSE (
-  echo %DEPLOYMENT_TEMP%\packages.config.js
-  goto error
-)
 
-
-:: 4. KuduSync
+:: 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
+:: 4. Call gulp
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  pushd %DEPLOYMENT_TARGET%
+  echo Execute gulp
+  call gulp
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+) ELSE (
+  echo %DEPLOYMENT_TARGET%\packages.config.js
+  goto error
+)
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
